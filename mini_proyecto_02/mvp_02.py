1793cb934d9b9cbd59b56e46985325db6673b070
Alejandro Meyer <alejomeyerr@gmail.com>
2026-03-29 19:52:37 -0500

    feat: add Fahrenheit to Celsius unit converter script

--- mini_proyecto_02/mvp_02.py
+++ mini_proyecto_02/mvp_02.py
@@ -6,11 +6,11 @@ print("Bienvenid@ al conversor de temperatura")
 # Paso 1: Solicitar ciudad
 ciudad_usuario = str(input("Indique la ciudad en donde se encuentra: "))
 
-# Paso 1: Solicitud de temperatuda en F°
+# Paso 2: Solicitud de temperatuda en F°
 grados_f = int(input("Indique los grados en Fahrenheit: "))
 
-# Paso 2: Convertir F° a C°
+# Paso 3: Convertir F° a C°
 grados_c = (grados_f - 32) / 1.8
 
-# Paso 3: Indicar resultado
+# Paso 4: Indicar resultado
 print(f"La ciudad de {ciudad_usuario} actualmente está a {grados_c}° Celsius")
