52bdf3060ca34cfcf70c86ffc942830da26cac23
Alejandro Meyer <alejomeyerr@gmail.com>
2026-03-30 12:48:52 -0500

    feat: implement number guessing game MVP and add project documentation

---
+++ mini_proyecto_03/README.md
@@ -0,0 +1,12 @@
+# El Juego de Adivinar el Número
+
+La computadora elige un número al azar entre 1 y 50, y el usuario tiene que adivinarlo. El programa debe decir si el número es "más alto" o "más bajo" hasta que el usuario acierte.
+
+* Conceptos: Importar módulos (import random), bucles while y comparaciones.
+
+* Fecha Inicio: 30/03/2026
+* Fecha Fin: /03/2026
+
+## Licencia
+
+~ Este MVP esta bajo la licencia MIT.
---
+++ mini_proyecto_03/mvp_03.py
@@ -0,0 +1,31 @@
+# El Juego de Adivinar el Número
+
+# Preparación del juego
+import random
+
+# Bienvenida
+print("Bienvenid@ a este espacio, aquí pondras a prueba tu habilidad para adivinar mi mente")
+
+# Paso 1: Explicar las reglas
+print("Yo escogeré un numero al azar entre el 1 al 50, tu misión es adivinar cual número he escogido.")
+print("¿Estás list@?")
+
+# Paso 2: Escoger el número
+numero_seleccionado = random.randint(1, 50)
+
+# Paso 3: Iniciar el juego
+while True:
+	respuesta_jugador = int(input("¿Que número estoy pensado?: "))
+
+	if respuesta_jugador == numero_seleccionado:
+		print("¡Correcto! Eres un(a) dios(a), dejame alabarte por siempre.")
+		break
+	
+	elif respuesta_jugador > numero_seleccionado:
+		print("Nop, mi número es menor al que pensaste, intentalo nuevamente")
+	else:
+		print("¡Casi!, mi número es mayor al que pensaste, intentalo nuevamente")
+		continue
+
+print("Gracias por jugar conmigo un rato, eres libre de seguir tu camino hacia el éxtio.")
+print("Bye bye")
